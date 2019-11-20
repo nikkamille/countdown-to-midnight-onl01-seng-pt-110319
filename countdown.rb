072292456c118f39770a7c4fbdcf7339e539d9c6
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-20 07:53:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(number)
   end
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  number = 10  
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end
+  puts "HAPPY NEW YEAR!"
+end
